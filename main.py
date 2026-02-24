3bef7d9e3b2b23677a3432f10ede9ffbca479658
tallen747 <TA747@pm.me>
2026-02-23 20:51:39 -0800

    Black Square Screen w/ Exit Working!

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Inifinite Loop!
+    while(True):
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            pass
+
+        screen.fill("black")
+
+        #Refresh the screen - last!
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
